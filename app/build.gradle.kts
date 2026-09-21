8b09e649c85c9ef3af5fb71adc20a55cd4aaa003
ZenO <kkeypop3750@gmail.com>
2026-09-21 15:33:45 +0100

    chore: bump version to 0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
 
     defaultConfig {
         applicationId = "com.goveye.app"
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.2.0"
     }
 
     testOptions {
